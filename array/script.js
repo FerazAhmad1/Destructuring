e538acb9b20dbeded8dc7b0d416377828c9f7398
Feraz Khan <ferazahmadgit62@gmail.com>
2024-01-27 18:19:42 +0530

    simple destructing of array

---
+++ array/script.js
@@ -0,0 +1,12 @@
+"use-strict";
+
+const arr = [1, 2, 3];
+
+// simple array destructuring
+
+const [a, b, c] = arr;
+console.log(a, b, c);
+
+const categories = ["pizza", "chinese", "Turkish", "Italian", "Arbian"];
+const [first, second] = categories; // taking first two value from categories array
+console.log(first, second);
