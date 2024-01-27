600b1741a0b79b43842d1710b20066b6f40cd639
FerazAhmad1 <112807125+FerazAhmad1@users.noreply.github.com>
2024-01-28 01:47:41 +0530

    nested array destruturing with default values

--- array/script.js
+++ array/script.js
@@ -19,3 +19,10 @@ let [, secondary, tereray] = categories; // secondary =
 // swapping using destructuring
 
 [secondary, tereray] = [tereray, secondary];
+const [i, , [j, k]] = [1, 2, [3, 4]]; //skiping 1 index
+console.log(i, j, k);
+
+// destructuring with default values
+
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r); // p=8,q=9,r= 1; if we not pass deafult values then r = undefined
