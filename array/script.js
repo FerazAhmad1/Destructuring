a989c650e4a663a6212c96ede7eb4e03827f1b80
Feraz Khan <ferazahmadgit62@gmail.com>
2024-01-27 19:19:22 +0530

    put a gap during array destructuring

--- array/script.js
+++ array/script.js
@@ -9,4 +9,7 @@ console.log(a, b, c);
 
 const categories = ["pizza", "chinese", "Turkish", "Italian", "Arbian"];
 const [first, second] = categories; // taking first two value from categories array
+// we can leave a hole with space and comma
+const [, secondary, tereray] = categories; // secondary =
+console.log(secondary, tereray);
 console.log(first, second);
